85f93d862bb44f549a6b09765399c9771be7ee49
Stanislav Vysotskyi <ardenhide@gmail.com>
2023-11-20 12:33:45 +0300

    - add `ToShortAddress` methods

--- src/Net.Web3.EthereumWallet/Extensions/AddressExtensions.cs
+++ src/Net.Web3.EthereumWallet/Extensions/AddressExtensions.cs
@@ -37,4 +37,29 @@ public static class AddressExtensions
     {
         return value.All(c => c is >= '0' and <= '9' or >= 'a' and <= 'f' or >= 'A' and <= 'F');
     }
+
+    /// <summary>
+    /// Shortens an Ethereum address by retaining only a specified number of characters at the beginning and end of the address, 
+    /// and replacing the middle part with ellipses (...). This method is useful for displaying a concise version of Ethereum addresses.
+    /// <example>
+    /// <code>
+    /// string ethAddress = "0x0000000000000000000000000000000000000000";
+    /// string shortAddress = ethAddress.ToShortAddress(4); // Outputs "0x0000...0000"
+    /// </code>
+    /// </example>
+    /// </summary>
+    /// <param name="address">The Ethereum address string to be shortened. Expected to start with '0x' followed by 40 hexadecimal characters.</param>
+    /// <param name="visibleChars">The number of characters to show at the beginning and end of the address after the '0x' prefix. Defaults to 4.</param>
+    /// <remarks>
+    /// The method ensures that the '0x' prefix and the specified number of characters from both the start and the end of the address are visible.
+    /// If the address length is shorter than the expected format, or if the visible characters exceed the address length, the original address is returned.
+    /// </remarks>
+    /// <returns>
+    /// A shortened version of the Ethereum address string with the '0x' prefix, the specified number of characters visible at the start and end, 
+    /// and the middle part replaced with ellipses (...).
+    /// </returns>
+    public static string ToShortAddress(this string address, int visibleChars = 4)
+    {
+        return $"{address[..(2 + visibleChars)]}...{address[^visibleChars..]}";
+    }
 }
