fdd943a48b57a0dd2a21e2fa50d7df3f675a9d12
Stanislav Vysotskyi <ardenhide@gmail.com>
2025-01-06 17:57:11 +0300

    Net.Web3.EthereumWallet: Custom json serializing

---
+++ src/Net.Web3.EthereumWallet.Json/Converters/EthereumAddressConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Net.Web3.EthereumWallet.Json.Converters
+{
+    public class EthereumAddressConverter : JsonConverter<EthereumAddress>
+    {
+        public override void WriteJson(JsonWriter writer, EthereumAddress? value, JsonSerializer serializer)
+        {
+            if (value?.Address == null)
+            {
+                writer.WriteNull();
+                return;
+            }
+
+            writer.WriteValue(value.Address);
+        }
+
+        public override EthereumAddress ReadJson(JsonReader reader, Type objectType, EthereumAddress? existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.Null)
+            {
+                throw new JsonSerializationException($"Cannot convert null value to {nameof(EthereumAddress)}.");
+            }
+
+            if (reader.TokenType != JsonToken.String)
+            {
+                throw new JsonSerializationException($"Cannot convert not string value to {nameof(EthereumAddress)}.");
+            }
+
+            return new EthereumAddress((string)reader.Value!);
+        }
+    }
+}
