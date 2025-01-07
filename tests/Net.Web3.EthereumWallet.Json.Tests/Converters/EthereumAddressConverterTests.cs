12a90e3e3a415641de18821c2f1466f9dcf05110
Stanislav Vysotskyi <66203238+ArdenHide@users.noreply.github.com>
2025-01-07 12:28:27 +0300

    Net.Web3.EthereumWallet: Custom json serializing (#26)
    * Net.Web3.EthereumWallet: Custom json serializing
    
    * - update converter
    - write tests

---
+++ tests/Net.Web3.EthereumWallet.Json.Tests/Converters/EthereumAddressConverterTests.cs
@@ -0,0 +1,44 @@
+﻿using Xunit;
+using Newtonsoft.Json;
+using FluentAssertions;
+using Net.Web3.EthereumWallet.Json.Converters;
+
+namespace Net.Web3.EthereumWallet.Json.Tests.Converters;
+
+public class EthereumAddressConverterTests
+{
+    public class Integration
+    {
+        internal class TestInput
+        {
+            [JsonConverter(typeof(EthereumAddressConverter))]
+            public EthereumAddress UserAddress { get; set; } = EthereumAddress.ZeroAddress;
+
+            [JsonConverter(typeof(EthereumAddressConverter))]
+            public EthereumAddress? NullableUserAddress { get; set; } = null;
+        }
+
+        internal static string JsonStringOfTestInput => @"{""UserAddress"":""0x0000000000000000000000000000000000000000"",""NullableUserAddress"":null}";
+
+        [Fact]
+        internal void WriteJson_ShouldReceiveExpectedJsonString()
+        {
+            var obj = new TestInput();
+
+            var stringJson = JsonConvert.SerializeObject(obj, Formatting.None);
+
+            stringJson.Should().Be(JsonStringOfTestInput);
+        }
+
+        [Fact]
+        internal void ReadJson_ShouldReceiveExpectedObject()
+        {
+            var stringJson = JsonStringOfTestInput;
+
+            var obj = JsonConvert.DeserializeObject<TestInput>(stringJson);
+
+            var expected = new TestInput();
+            obj.Should().BeEquivalentTo(expected);
+        }
+    }
+}
